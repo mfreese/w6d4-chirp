caf0cf0023c4187f20cbd73bff6b4d6d118087c8
Michael Freese <mrfreese1@gmail.com>
2016-11-04 17:42:17 -0400

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# 50.times do
-#   user = User.create!(
-#   email: Faker::Internet.free_email,
-#   password: Faker::Internet.password
-#   )
-# end
+50.times do
+  User.create!(
+  email: Faker::Internet.free_email,
+  password: Faker::Internet.password
+  )
+end
 
-200.times do
+100.times do
   User.all.sample.posts << Post.create!(
     chirp:  Faker::Company.catch_phrase
     )
