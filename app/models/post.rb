04bc49f243b0b8967805495e9236c12bbb38f1cb
Michael Freese <mrfreese1@gmail.com>
2016-11-04 17:36:33 -0400

    seed

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,14 +2,12 @@ class Post < ApplicationRecord
   belongs_to :user
   validates :chirp,
         length: {maximum: 170},
-        presence: true,
+        presence: true
 
 
-
-                  def self.timeline(user)
-                    following_ids = user.followees(User).pluck(:id)
-                    all_ids= following_ids << user.id
-                    Post.where(user_id: all_ids).order("created_at DESC")
-                  end
-
+  def self.timeline(user)
+      following_ids = user.followees(User).pluck(:id)
+      all_ids = following_ids << user.id
+      Post.where(user_id: all_ids).order("created_at DESC")
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-50.times do
-  user = User.create!(
-  email: Faker::Internet.free_email,
-  password: Faker::Internet.password
-  )
-end
+# 50.times do
+#   user = User.create!(
+#   email: Faker::Internet.free_email,
+#   password: Faker::Internet.password
+#   )
+# end
 
-100.times do
+200.times do
   User.all.sample.posts << Post.create!(
     chirp:  Faker::Company.catch_phrase
     )
