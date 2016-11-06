8070b5c80cf94c0574d4181e73c45473e52a689a
Michael Freese <mrfreese1@gmail.com>
2016-11-05 21:21:38 -0400

    changes-following

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,15 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :avatar, :api_token, :name
+  attributes :id, :email, :avatar, :api_token, :name, :following
 
   def avatar
     Refile.attachment_url(object, :avatar, :fit, 800, 800, format: "jpg")
   end
+
+  def following
+    if current_user
+      object.followed_by?(current_user)
+    else
+      false
+    end
+  end      
 end
