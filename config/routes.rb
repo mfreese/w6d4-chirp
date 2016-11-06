1310c33736e72c601aa290b3fc66ed2335fef50a
Michael Freese <mrfreese1@gmail.com>
2016-11-06 14:14:40 -0500

    routes change

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
    post '/log_in' => 'users#log_in'
    get '/follow' => 'users#follow'
    get '/unfollow' => 'users#unfollow'
-   get '/all_followers' => 'users#all_follow'
+   get '/all_followers' => 'users#all_followers'
    get '/user/me' => 'users#show_user'
 
 end
