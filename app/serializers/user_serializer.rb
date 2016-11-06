e544ba1b82e3e8f18e4c0eeed185aa0876c42731
Michael Freese <mrfreese1@gmail.com>
2016-11-06 11:18:24 -0500

    posts

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :avatar, :api_token, :name, :following, :followers_count, :followees_count
 
+  has_many :posts
+
   def avatar
     Refile.attachment_url(object, :avatar, :fit, 800, 800, format: "jpg")
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :users
    post '/log_in' => 'users#log_in'
    get '/follow' => 'users#follow'
-   get 'unfollow' => 'users#unfollow'
+   get '/unfollow' => 'users#unfollow'
    get '/all_followers' => 'users#all_follow'
    get '/user/me' => 'users#show_user'
 
